22cc43c853bd41897d9018f11b07024edf4c35fa
miura <noblesse-oblige_23@i.softbank.jp>
2016-06-26 17:30:50 +0900

    feat(npm): install vueify

---
+++ components/message.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>{{message}}</p>
+</template>
+
+<script>
+// babel を入れているので ES6 で書ける
+export default {
+  data() {
+    return {
+      message: "Hello, Vue!"
+    }
+  }
+}
+</script>
