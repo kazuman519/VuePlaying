ba4ddaf3936231e886cbd544cc43c5b0e20a9290
miura <noblesse-oblige_23@i.softbank.jp>
2016-06-26 19:01:59 +0900

    feat(component): create task_list

---
+++ components/task_list.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>{{list.title}}</div>
+</template>
+
+<script>
+  var TaskList = require('../models/task_list').default
+
+  export default {
+    props: {
+      list: {
+        type: Object,
+        default: function () {
+          return new TaskList
+        }
+      }
+    }
+  }
+</script>
--- main.js
+++ main.js
@@ -2,9 +2,16 @@
  * Created by kazuma on 2016/06/26.
  */
 var Vue = require('vue')
+var TaskList = global.TaskList = require('./models/task_list').default
+
+var task_list = global.task_list = new TaskList
+
 new Vue({
   el: '#app',
+  data: {
+    list: task_list,
+  },
   components: {
-    MyMessage: require('./components/message.vue')
+    MyTaskList: require('./components/task_list.vue')
   }
 })
